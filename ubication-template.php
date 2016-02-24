f8b1664bfab782ecfcd51abf7c1065fb32b0c249
Angel Chanquin <ajchanquin@gmail.com>
2016-02-24 14:50:46 -0600

    Renamed template, fixed title

--- ubication-template.php
+++ ubication-template.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Continentes List
+ * Template Name: Ubication
  */
 
     get_header();
@@ -15,13 +15,15 @@
             <section class="container">
                 <div class="row">
                     <div class="title">
+                        <h1>
                         <?php 
                             if ($continent == null || $continent == "") {
-                                echo "<h2>Por Ubicación</h2>";
+                                the_title();
                             } else {
-                                echo "<h2>$continent</h2>";
+                                echo $continent;
                             }
                         ?>
+                        </h1>
                     </div>
                 </div>
             </section>
@@ -32,13 +34,15 @@
                 <div class="row">
                     <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
                         <div class="content-panel-title">
+                            <h2>
                             <?php 
                                 if ($continent == null || $continent == "") {
-                                    echo "<h2>Continentes</h2>";
+                                    echo "Continentes";
                                 } else {
-                                    echo "<h2>Paises</h2>";
+                                    echo "Paises";
                                 }
                             ?>
+                            </h2>
                         </div>
                     </div>
                     <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 blog">
@@ -48,12 +52,12 @@
                                         if ($continent == null || $continent == "") {
                                             $continentes = $wpdb->get_results("SELECT DISTINCT continentName FROM stations ORDER BY continentName");
                                             foreach ($continentes as $continente) {
-                                            echo "<li><a href=\"por-ubicacion?continent=$continente->continentName\">$continente->continentName</a></li>";
+                                                echo "<li><a href=\"por-ubicacion?continent=$continente->continentName\">$continente->continentName</a></li>";
                                             }
                                         } else {
                                             $paises = $wpdb->get_results("SELECT DISTINCT countryName FROM stations WHERE continentName = '$continent' ORDER BY countryName");
                                             foreach ($paises as $pais) {
-                                            echo "<li><a href=\"radios?continent=$continent&country=$pais->countryName\">$pais->countryName</a></li>";
+                                                echo "<li><a href=\"radios?continent=$continent&country=$pais->countryName\">$pais->countryName</a></li>";
                                             }
                                         }
                                     ?>
